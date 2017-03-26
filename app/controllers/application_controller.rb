1ab7fbdad05a4885045afc60e5f86f7444e2d6a5
StanisLoveSid <joingamings@gmail.com>
2017-03-27 02:57:01 +0300

    delivery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,18 @@ class ApplicationController < ActionController::Base
 
   def current_order
     if !session[:order_id].nil?
-      !find_order.in_progress? && !find_order.filled? ? Order.new(number: number_generator) : find_order
+      !find_order.in_progress? && !find_order.filled? ? Order.new(number: number_generator, delivery: find_delivery) : find_order
     else
-      Order.new(number: number_generator)
+      Order.new(number: number_generator, delivery: find_delivery)
     end
   end
 
   def number_generator
     "#N"+Time.now.to_f.to_s[0, 10]
+  end
+
+  def find_delivery
+    Delivery.find_by(name: "none")
   end
 
   def find_order
